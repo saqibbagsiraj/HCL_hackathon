5161a9483d0646fc292d5588d8a6b71556f5a72e
shash <shashankshekharrai5629@gmail.com>
2026-04-13 12:15:32 +0530

    intialized springboot structure

---
+++ Food_Ordering_App/src/test/java/com/food_ordering_app/food_ordering_app/FoodOrderingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_ordering_app.food_ordering_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
